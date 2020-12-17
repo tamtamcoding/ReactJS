d46c355a902b32972d39e06dc84e41a15b5d1196
RustamArifin <72663154+tamtamcoding@users.noreply.github.com>
2020-12-17 10:36:09 +0800

    Add files via upload

---
+++ cocktail/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad ipsam quaerat dolores? Repellendus ullam voluptates autem ipsa quasi tempore impedit tempora. Et aperiam commodi minus! Repudiandae voluptate, nisi pariatur asperiores magni hic consectetur! Nihil quos reiciendis quam hic. Quo neque consequatur voluptatum optio earum sunt nam facilis quam odio iste.</p>
+    </section>
+  )
+}
+
+export default About
